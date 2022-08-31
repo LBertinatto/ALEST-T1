229bbc8ff45a2451afa68b7ee4ed57490cfd10cf
Leonardo Bertinatto <105060693+LBertinatto@users.noreply.github.com>
2022-08-30 22:02:44 -0300

    IsEmpty
    Contagem de tempo de execução do método IsEmpty

---
+++ IsEmpty2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class IsEmpty2 {
+    public static void main(String args[]){
+        ArrayList<String> set = new ArrayList<>();
+
+        for(int i = 0; i<13; i++ ){
+        set.isEmpty();
+        }
+
+        double tempo = System.currentTimeMillis();
+        System.out.println(tempo/1000);
+        //Unidade de medida: segundos
+        //Runed in a MacBook Pro with M1 Chip
+    }
+}
+
