80398cdfbb33a0ee89303a1f50b0d547fdd9d243
Leonardo Bertinatto <105060693+LBertinatto@users.noreply.github.com>
2022-08-30 22:05:37 -0300

    Update IsEmpty2.java

--- IsEmpty2.java
+++ IsEmpty2.java
@@ -1,5 +1,10 @@
 import java.util.*;
-
+/*
+ * “The size, isEmpty, get, set, iterator, and listIterator operations run in constant time. 
+ * The add operation runs in amortized constant time, that is, adding n elements requires O(n) time. 
+ * All of the other operations run in linear time (roughly speaking). The constant factor is low compared
+ *  to that for the LinkedList implementation.” Fonte: ORACLE, Java SE API [4]
+ */
 public class IsEmpty2 {
     public static void main(String args[]){
         ArrayList<String> set = new ArrayList<>();
