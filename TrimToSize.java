cc489883f1d4ca9e872033db14cedb4b8993c39d
Leonardo Bertinatto <105060693+LBertinatto@users.noreply.github.com>
2022-08-31 00:38:00 -0300

    TrimToSize
    Testes de contagem de tempo utilizando o método TrimToSize

---
+++ TrimToSize.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class TrimToSize{
+
+    public static void main(String[] args) {
+      ArrayList<Integer> set = new ArrayList<Integer>(14);
+      set.add(1);
+      set.trimToSize();
+
+      double tempo = System.currentTimeMillis();
+        System.out.println(tempo/1000);
+
+        /*
+        * Colocou-se um elementono ArrayList para inicialização
+        *
+        * Testes de tempo foram feitos aumentando o tamanho do ArrayList inicial, 
+        * considerando assim a quantidade de sizes a serem decrementados do tamanho do 
+        * ArrayList durante a execução do método
+        *
+        * Unidade de Medida: segundos
+        * Runed in a Macbook Pro with M1 Chip
+        */
+      }
+  }
+   
+  
+  
+    
+ 
